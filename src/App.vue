cdace7d489838d8a775aa28caea1611e0b2102f3
serinazunazuna <seririn1999@gmail.com>
2021-08-07 18:46:09 +0900

    todolist作成

--- src/App.vue
+++ src/App.vue
@@ -6,59 +6,17 @@
         {{ message }}
       </div>
     </div>
+    <TodoList />
   </div>
 </template>
 
 <script>
+import TodoList from "@/components/TodoList.vue"
 export default {
   name: "App",
-  data: function () {
-    return {
-      message: "WebExpert Course Vue Template",
-    }
+  components: {
+    TodoList,
   },
-  methods: {},
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-</style>
-
-<style scoped>
-.hello-wrold {
-  margin-top: 60px;
-  text-align: center;
-}
-
-.hello-wrold .hello-world__message {
-  color: #2c3e50;
-}
-</style>
-
-<template>
-  <div id="app">
-    <div class="hello-wrold">
-      <img alt="Vue logo" src="./assets/logo.png" />
-      <div class="hello-wrold__message">
-        {{ message }}
-      </div>
-    </div>
-    **<TodoList />**
-  </div>
-</template>
-
-<script>
-**import TodoList from "@/components/TodoList.vue"**
-export default {
-  name: "App",
-  **components: {
-    TodoList
-  },**
   data: function () {
     return {
       message: "WebExpert Course Vue Template",
